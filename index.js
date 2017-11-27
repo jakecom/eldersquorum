46794eea41a13319eb5c6d130d731ddfb2f7a68e
Jake Hedgecock <hed15002@byui.edu>
2017-11-27 14:42:06 -0800

    database

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 // fake posts to simulate a database
 const posts = [
@@ -38,14 +37,17 @@ const posts = [
   }
 ]
 
+app.set('view engine', 'ejs');
+
+
 /*app.get('/', function(request, response) {
   response.render('pages/index')
 });*/
 
 // blog home page
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
   // render `home.ejs` with the list of posts
-  res.render('/home', { posts: posts })
+  res.render('views/home', { posts: posts })
 })
 
 app.get('/cool', function(request, response) {
