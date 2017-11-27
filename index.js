b15d708c39251c3ead0c0e7df7d6256bb9b72138
Jake Hedgecock <hed15002@byui.edu>
2017-11-26 20:07:19 -0800

    database

--- index.js
+++ index.js
@@ -27,25 +27,14 @@ var pg = require('pg');
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    
-    client.query('SELECT * FROM family', function(err, result) {
-    	done();
-    	if (err)
-    		{ console.error(err); response.send("Error" + err); }
-    	else{
-    		response.render('pages/db', {results: result.rows} );
-    		client.query('SELECT id, first, last FROM person WHERE id=family(id)', function(err, result) {
+    client.query('SELECT id, first, last FROM person', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else{
  response.render('pages/db', {results: result.rows} ); 
-      }
-
-       
+      }  
     });
-    	}
-    })
   });
 });
 
