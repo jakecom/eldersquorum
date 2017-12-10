ea602f4f334d3db91e6f389c68bcb7ab55665a50
Jake Hedgecock <hed15002@byui.edu>
2017-12-10 12:57:45 -0800

    b

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ var pg = require('pg');
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT id, first, last FROM person', function(err, result) {
+    client.query('SELECT id, first, last FROM person ORDER BY id', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
@@ -170,7 +170,7 @@ app.get('/db', function (request, response) {
 
 app.get('/report', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT id, first, last FROM person', function(err, result) {
+    client.query('SELECT id, first, last FROM person ORDER BY id', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
