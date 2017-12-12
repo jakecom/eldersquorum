c39941958a3e5882f0a5efc4681b09cbe0533cfc
Jake Hedgecock <hed15002@byui.edu>
2017-12-11 22:17:19 -0800

    close

--- index.js
+++ index.js
@@ -41,8 +41,6 @@ app.set('view engine', 'ejs');
 
 
 
-
-// fake posts to simulate a database
 const posts = [
   {
     id: 1,
@@ -190,12 +188,12 @@ if (request.session.user){
 
 // blog post
 app.get('/post/:id', (request, response) => {
-  // find the post in the `posts` array
+  
   const post = posts.filter((post) => {
     return post.id == request.params.id
   })[0]
 
-  // render the `post.ejs` template with the post content
+  
   response.render('pages/post.ejs', {
     author: post.author,
     title: post.title,
